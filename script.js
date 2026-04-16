a7c7c4f216614eac04e1283551b15d75b271cb70
Patel Umangi <202098646+Umangi-webdev@users.noreply.github.com>
2026-04-16 19:49:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// ========================= script.js =========================
+function showQuizMessage() {
+  document.getElementById("quizMessage").innerText = "Quiz started! Best of luck 🎯";
+}
+
+function markAttendance() {
+  document.getElementById("attendanceStatus").innerText = "Status: Present ✅";
+}
